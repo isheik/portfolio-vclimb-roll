afcfe7d767187d28f5a41a68b51b59479ddc2e4a
Keishi Asai <isheik808@gmail.com>
2021-07-15 22:17:51 -0700

    add ogp settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,47 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+      { hid: "description", name: "description", content: "KEI's Portfolio" },
+      {
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "KEI's Portfolio"
+      },
+      { hid: "og:type", property: "og:type", content: "website" },
+      { hid: "og:url", property: "og:url", content: "https://isheik.dev" },
+      { hid: "og:title", property: "og:title", content: "KEI's Portfolio" },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "KEI's Portfolio"
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "/portfolio_ogp_image.jpg"
+      },
+      { name: "twitter:site", content: "@isheik8" },
+      { name: "twitter:card", content: "summary_large_image" },
+      {
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://isheik.dev"
+      },
+      {
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "KEI's Portfolio"
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content: "KEI's Portfolio"
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "/portfolio_ogp_image.jpg"
+      }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
